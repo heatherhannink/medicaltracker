4ba0ed623e2ffbe4aecb7d07348c4a8fee8b9d27
KEDell <sosha98@live.com>
2024-01-18 21:01:55 -0500

    Reset Input Form

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,3 @@
-
-
-
-
 const postMedications = async(medicationObj) => {
     const response = await fetch('/api/medications', {
         method: 'POST',
@@ -33,48 +29,7 @@ const clickHandler = (event) => {
 console.log(dose.value)
 const newMed ={name:med.value,dosage:dose.value}
 postMedications(newMed)
+document.getElementById("med", "dose").reset();
 }   
 
     button.addEventListener('click', clickHandler)
-    // console.log(buttonArr)
-
-    // window.localStorage.setItem("buttonArr", JSON.stringify(buttonArr));
-    // medArr.appendChild(paragraph);
-    // const paragraph = document.createElement("p");
-// paragraph.innerText = date;
-// const text = localStorage.getItem(medArr[i].id)
-//     if(text){
-//         console.log(text)
-//         medArr[i].children[1].value=text
-//     }
-// }
-    
-    
-// 
-//     const text = event.target.previousElementSibling.value
-//     const parentID = event.target.parentNode.id
-//     console.log (text)
-//     console.log (parentID)
-//     localStorage.setItem(parentID, text)
-
-// }
-
-
-
-// // get data from local storage
-
-// // loop
-
-// // localStorage.getItem()
-
-// //textAreaArr[i].value = whatever came back from local
-
-// var currentHour = dayjs().hour();
-// let date = dayjs().format ("DD, MMMM, YYYY");
-// console.log (date);
-// //current day is displayed at top when you open//
-// let currentDay = document.querySelector ('#currentDay');
-
-// const paragraph = document.createElement("p");
-// paragraph.innerText = date;
-// currentDay.appendChild(paragraph);
