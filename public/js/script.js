e6baae8c674a41bc4f009bd0fe688bcd7641fc79
Melody <meliaukea@gmail.com>
2024-01-18 21:08:03 -0500

    Created a function that appends paragraphs to the page, but it's currently not working

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,3 @@
-
-
-
-
 const postMedications = async(medicationObj) => {
     const response = await fetch('/api/medications', {
         method: 'POST',
@@ -30,51 +26,24 @@ console.log(med)
 console.log(dose)
 const clickHandler = (event) => {
     console.log(med.value)
-console.log(dose.value)
-const newMed ={name:med.value,dosage:dose.value}
-postMedications(newMed)
+    console.log(dose.value)
+    const newMed ={name:med.value,dosage:dose.value}
+    postMedications(newMed)
 }   
 
-    button.addEventListener('click', clickHandler)
-    // console.log(buttonArr)
+button.addEventListener('click', clickHandler)
 
-    // window.localStorage.setItem("buttonArr", JSON.stringify(buttonArr));
-    // medArr.appendChild(paragraph);
-    // const paragraph = document.createElement("p");
-// paragraph.innerText = date;
-// const text = localStorage.getItem(medArr[i].id)
-//     if(text){
-//         console.log(text)
-//         medArr[i].children[1].value=text
-//     }
-// }
-    
-    
-// 
-//     const text = event.target.previousElementSibling.value
-//     const parentID = event.target.parentNode.id
-//     console.log (text)
-//     console.log (parentID)
-//     localStorage.setItem(parentID, text)
+// Appends medication and dosage to the page when a user enters it into the database
+let medList = document.querySelector('#input-group')
 
-// }
+const addToList = (event) => {
+    event.preventDefault()
 
+    const medP = document.createElement('p')
+    medP.textContent = med.value
+    medList.appendChild(medP)
+    document.querySelector('#Medication').value = ""
+    document.querySelector('#Dosage').value = ""
+}
 
-
-// // get data from local storage
-
-// // loop
-
-// // localStorage.getItem()
-
-// //textAreaArr[i].value = whatever came back from local
-
-// var currentHour = dayjs().hour();
-// let date = dayjs().format ("DD, MMMM, YYYY");
-// console.log (date);
-// //current day is displayed at top when you open//
-// let currentDay = document.querySelector ('#currentDay');
-
-// const paragraph = document.createElement("p");
-// paragraph.innerText = date;
-// currentDay.appendChild(paragraph);
+button.addEventListener('click', addToList)
